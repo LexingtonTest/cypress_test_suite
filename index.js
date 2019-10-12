acf9592dfdd289a429b1bf92ac5e7c6c4c2c59bc
LexingtonSmithe <lowndesy_3@hotmail.com>
2019-10-12 23:47:33 +0100

    first test - up to 2/6

---
+++ index.js
@@ -0,0 +1,10 @@
+function addTwoNumbers(name, email){
+    var c = name + email;
+    console.log(c);
+}
+
+
+addTwoNumbers(3, 6);
+addTwoNumbers(9, 68);
+addTwoNumbers(143, 1);
+addTwoNumbers(345457, 985);
